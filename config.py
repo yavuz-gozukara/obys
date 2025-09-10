62cf95eb5ff8aba120482798bb645731b70f5044
yavuz selim gozukara <78623551+yavuz-gozukara@users.noreply.github.com>
2025-09-10 22:12:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ class Config:
     Uygulama ayarlarını ve ortam değişkenlerini tutar.
     """
     SECRET_KEY = os.environ.get('SECRET_KEY', 'sizin_gizli_anahtarınız_buraya_gelecek')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///site.db').replace('postgres://site_2p6b_user:YRqQKAvWG5bucK3FaX93lIj6sLsyrqZ5@dpg-d26lkfggjchc73e3stu0-a/site_2p6b', 'postgresql://site_2p6b_user:YRqQKAvWG5bucK3FaX93lIj6sLsyrqZ5@dpg-d26lkfggjchc73e3stu0-a/site_2p6b')
+   db_uri = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
+if db_uri and db_uri.startswith("postgres://"):
+    db_uri = db_uri.replace("postgres://", "postgresql://", 1)
+SQLALCHEMY_DATABASE_URI = db_uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'uploads'
     ALLOWED_EXTENSIONS = {'csv', 'xlsx', 'xls'}
